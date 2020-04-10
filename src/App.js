348049c9711a68dabb3b26dfe1b2618c2703face
Thiago Costa <thiagonsc.costa@hotmail.com>
2020-04-10 03:24:30 -0300

    useEffect

--- src/App.js
+++ src/App.js
@@ -1,16 +1,33 @@
 import React, { useState, useEffect } from 'react';
+import { MdFavorite } from 'react-icons/md';
+import { MdFavoriteBorder } from "react-icons/md";
 
 function App() {
 
-  useEffect(async () => {
-    const res = await fetch('https://api.github.com/users/thiagonscosta/repos');
-    const data = await res.json();
-    setMyRepos(data);
+  const [myRepos, setMyRepos] = useState([]);
+
+  useEffect(() => {
+    async function fetchMyRepos() {
+      const res = await fetch('https://api.github.com/users/thiagonscosta/repos');
+      const data = await res.json();
+      setMyRepos(data);
+    }
+    fetchMyRepos();
   }, []);
 
-  const [repoName, setRepoName] = useState('');
+  function handleFavorite(id) {
+    const favRepos = myRepos.map(repo => {
+      return repo.id === id ? { ...repo, favorite: !repo.favorite } : repo
+    });
 
-  const [myRepos, setMyRepos] = useState([]);
+    setMyRepos(favRepos);
+  }
+
+  useEffect(() => {
+
+  }, [myRepos])
+
+  const [repoName, setRepoName] = useState('');
 
   const [repositories, setRepositories] = useState([
     {_id: 1, name: 'repo-1'},
@@ -27,27 +44,44 @@ function App() {
   }
 
   return (
-    <div>
-      <p>Adicionar novo repositório</p>
-      <form onSubmit={handleAddRepository}>
-        <input 
-          type="text" 
-          placeholder="Nome"
-          value={repoName} 
-          onChange={e => setRepoName(e.target.value)}
-        />
-        <button type="submit">Adicionar</button>
-      </form>
-      {repositories.map(r => (
-        <li key={r._id}>{r.name}</li>
-      ))}
+    <main>
+      <div className="content">
+        <div className="content-wrap">
+          <h4>Adicionar novo repositório</h4>
+          <form onSubmit={handleAddRepository}>
+            <input
+              type="text"
+              placeholder="Nome"
+              value={repoName}
+              onChange={(e) => setRepoName(e.target.value)}
+            />
+            <button type="submit">Adicionar</button>
+          </form>
+          <ul>
+            {repositories.map((r) => (
+              <li key={r._id}>{r.name}</li>
+            ))}
+          </ul>
+        </div>
 
-      <p>My Repos</p>
-      {myRepos.map(r => (
-        <li key={r.id}>{r.name}</li>
-      ))}
-    </div>
-
+        <div className="content-wrap">
+          <h4>My Repos</h4>
+          <ul className="myrepos-list">
+            {myRepos.map((r) => (
+              <li key={r.id}>
+                <div>
+                  <p>{r.name}</p>
+                  <button onClick={() => handleFavorite(r.id)}>
+                    {r.favorite && <MdFavorite color="#FF0000"/>}{" "}
+                    {!r.favorite && <MdFavoriteBorder />}
+                  </button>
+                </div>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </main>
   );
 }
 
